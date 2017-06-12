efe00a290dee765f7048725af4e725c1260b8bbb
Kasra Rahjerdi <hi@kasra.codes>
2017-06-12 15:00:49 -0600

    First commit

---
+++ KasraFbGamesTest/DirectXManager.cs
@@ -0,0 +1,267 @@
+﻿using D3D11 = SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using SharpDX.Direct3D;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace KasraFbGamesTest
+{
+    // DirectX powered Win8.1 and above screen duplication API.
+    // See http://www.virtualdub.org/blog/pivot/entry.php?id=356
+    class DirectXManager : IDisposable
+    {
+        private D3D11.Device d3dDevice;
+        private D3D11.DeviceContext d3dDeviceContext;
+        private SwapChain swapChain;
+
+        private OutputDuplication duplicatedOutput;
+        private D3D11.Texture2D screenTexture;
+        private Resource screenResource;
+        private SharpDX.DataStream screenDataStream;
+        private Surface screenSurface;
+
+        public bool IsRecording { get; private set; }
+        private Task recordingTask;
+        private OnFrameReady frameListener;
+
+        private BlockingCollection<FrameUpdateInfo> recordingQueue = new BlockingCollection<FrameUpdateInfo>(1);
+        private BlockingCollection<FrameUpdateInfo> processingQueue = new BlockingCollection<FrameUpdateInfo>(1);
+
+        public Rect GetMainMonitorSize()
+        {
+            MonitorInfo monitorInfo = new MonitorInfo();
+            monitorInfo.cbSize = Marshal.SizeOf(monitorInfo);
+            MonitorEnumProc callback = (IntPtr hDesktop, IntPtr hdc, ref Rect prect, int d) => {
+                GetMonitorInfo(hDesktop, ref monitorInfo);
+                return monitorInfo.dwFlags == 0;
+            };
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, callback, 0);
+            return monitorInfo.rcMonitor;
+        }
+
+        public void Init(IntPtr formHandle)
+        {
+            Rect size = GetMainMonitorSize();
+            ModeDescription backBufferDesc = new ModeDescription(size.width, size.height, new Rational(60, 1), Format.R8G8B8A8_UNorm);
+            SwapChainDescription swapChainDesc = new SwapChainDescription()
+            {
+                ModeDescription = backBufferDesc,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = Usage.RenderTargetOutput,
+                BufferCount = 1,
+                OutputHandle = formHandle,
+                IsWindowed = true
+            };
+
+
+            Factory1 factory = new Factory1();
+
+            D3D11.Device.CreateWithSwapChain(factory.Adapters[0], D3D11.DeviceCreationFlags.SingleThreaded, new FeatureLevel[] { FeatureLevel.Level_11_1 }, swapChainDesc, out d3dDevice, out swapChain);
+            d3dDeviceContext = d3dDevice.ImmediateContext;
+
+            // CPU accessible texture with _STAGING
+            D3D11.Texture2DDescription textureDesc = new D3D11.Texture2DDescription();
+            textureDesc.CpuAccessFlags = D3D11.CpuAccessFlags.Read;
+            textureDesc.BindFlags = D3D11.BindFlags.None;
+            textureDesc.Format = Format.B8G8R8A8_UNorm;
+            textureDesc.Height = size.height;
+            textureDesc.Width = size.width;
+            textureDesc.OptionFlags = D3D11.ResourceOptionFlags.None;
+            textureDesc.MipLevels = 1;
+            textureDesc.ArraySize = 1;
+            textureDesc.SampleDescription.Count = 1;
+            textureDesc.SampleDescription.Quality = 0;
+            textureDesc.Usage = D3D11.ResourceUsage.Staging;
+
+            screenTexture = new D3D11.Texture2D(d3dDevice, textureDesc);
+
+            // Actual duplication API
+            Output1 output = new Output1(factory.Adapters1[0].Outputs[0].NativePointer);
+            duplicatedOutput = output.DuplicateOutput(d3dDevice);
+        }
+
+        public BlockingCollection<FrameUpdateInfo> GetRecorder()
+        {
+            return recordingQueue;
+        }
+
+        public BlockingCollection<FrameUpdateInfo> GetProcessor()
+        {
+            return processingQueue;
+
+        }
+        public void StartRecording(OnFrameReady listener)
+        {
+            IsRecording = true;
+            frameListener = listener;
+            if (recordingQueue.Count == 0)
+                recordingQueue.Add(new FrameUpdateInfo());
+            recordingTask = new Task(new Action(Record));
+            recordingTask.Start();
+        }
+
+        public void StopRecording()
+        {
+            IsRecording = false;
+        }
+
+        private void Record()
+        {
+            while (IsRecording)
+            {
+                FrameUpdateInfo update = recordingQueue.Take();
+                OutputDuplicateFrameInformation duplicateFrameInformation;
+                try
+                {
+                    duplicatedOutput.AcquireNextFrame(1000, out duplicateFrameInformation, out screenResource);
+                }
+                catch (SharpDX.SharpDXException e)
+                {
+                    Console.WriteLine("Failed to acquire frame: {0}", e);
+                    if (e.ResultCode.Code == ResultCode.WaitTimeout.Result.Code)
+                    {
+                        // keep retrying
+                        continue;
+                    }
+                    else
+                    {
+                        // just gonna eat all errors for now
+                        Console.WriteLine(e);
+                        continue;
+                    }
+                }
+
+                if (duplicateFrameInformation.TotalMetadataBufferSize > 0)
+                {
+                    // get move rects
+                    int moveRectsLen = 0;
+                    OutputDuplicateMoveRectangle[] moveRects = new OutputDuplicateMoveRectangle[duplicateFrameInformation.TotalMetadataBufferSize];
+                    duplicatedOutput.GetFrameMoveRects(moveRects.Length, moveRects, out moveRectsLen);
+                    update.MovedRegions = new MovedRegion[moveRectsLen / Marshal.SizeOf(typeof(OutputDuplicateMoveRectangle))];
+                    for (int i = 0; i < update.MovedRegions.Length; i++)
+                    {
+                        update.MovedRegions[i] = new MovedRegion
+                        {
+                            start = new Point(moveRects[i].SourcePoint.X, moveRects[i].SourcePoint.Y),
+                            to = new Rect()
+                            {
+                                left = moveRects[i].DestinationRect.Left,
+                                top = moveRects[i].DestinationRect.Top,
+                                right = moveRects[i].DestinationRect.Right,
+                                bottom = moveRects[i].DestinationRect.Bottom
+                            }
+                        };
+                    }
+
+                    // get dirty rects
+                    int dirtyRectsLen = 0;
+                    SharpDX.Mathematics.Interop.RawRectangle[] dirtyRects = new SharpDX.Mathematics.Interop.RawRectangle[duplicateFrameInformation.TotalMetadataBufferSize];
+                    duplicatedOutput.GetFrameDirtyRects(dirtyRects.Length, dirtyRects, out dirtyRectsLen);
+                    update.DirtyRects = new Rect[dirtyRectsLen / Marshal.SizeOf(typeof(SharpDX.Mathematics.Interop.RawRectangle))];
+                    for (int i = 0; i < update.DirtyRects.Length; i++)
+                    {
+                        update.DirtyRects[i] = new Rect
+                        {
+                            left = dirtyRects[i].Left,
+                            top = dirtyRects[i].Top,
+                            right = dirtyRects[i].Right,
+                            bottom = dirtyRects[i].Bottom
+                        };
+                    }
+                }
+                else
+                {
+                    update.MovedRegions = new MovedRegion[0];
+                    update.DirtyRects = new Rect[0];
+                }
+
+                // copy resource into memory that can be accessed by the CPU
+                d3dDeviceContext.CopyResource(screenResource.QueryInterface<D3D11.Resource>(), screenTexture);
+                // cast from texture to surface, so we can access its bytes
+                screenSurface = screenTexture.QueryInterface<Surface>();
+                // map the resource to access it
+                screenSurface.Map(MapFlags.Read, out screenDataStream);
+
+                // Read it!
+                if (update.LastAcquiredFrame == null)
+                {
+                    update.LastAcquiredFrame = new Bitmap(screenTexture.Description.Width, screenTexture.Description.Height, PixelFormat.Format32bppArgb);
+                }
+                var BoundsRect = new Rectangle(0, 0, update.LastAcquiredFrame.Width, update.LastAcquiredFrame.Height);
+                BitmapData bmpData = update.LastAcquiredFrame.LockBits(BoundsRect, ImageLockMode.WriteOnly, update.LastAcquiredFrame.PixelFormat);
+
+                screenDataStream.Read(bmpData.Scan0, 0, bmpData.Stride * update.LastAcquiredFrame.Height);
+                update.LastAcquiredFrame.UnlockBits(bmpData);
+
+                // free resources
+                screenDataStream.Close();
+                screenSurface.Unmap();
+                screenSurface.Dispose();
+                if (screenResource != null)
+                    screenResource.Dispose();
+                duplicatedOutput.ReleaseFrame();
+
+                // Add to the queue, and we'll wait until we're needed again (hopefully)
+                processingQueue.Add(update);
+                frameListener();
+            }
+        }
+
+        public void Dispose()
+        {
+            swapChain.Dispose();
+            d3dDevice.Dispose();
+            d3dDeviceContext.Dispose();
+        }
+
+        // Delegate for "Frame ready to process"
+        public delegate void OnFrameReady();
+
+        // Models
+        public class FrameUpdateInfo
+        {
+            public Bitmap LastAcquiredFrame { get; set; }
+            public MovedRegion[] MovedRegions { get; set; }
+            public Rect[] DirtyRects { get; set; }
+        }
+
+        public struct MovedRegion
+        {
+            public Point start;
+            public Rect to;
+        }
+
+        // Windows SDK
+        [DllImport("user32")]
+        private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lpRect, MonitorEnumProc callback, int dwData);
+        [DllImport("user32")]
+        private static extern bool GetMonitorInfo(IntPtr hdc, ref MonitorInfo monitorInfo);
+
+        private delegate bool MonitorEnumProc(IntPtr hDesktop, IntPtr hdc, ref Rect pRect, int dwData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public int width { get { return right - left; } }
+            public int height { get { return bottom - top; } }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MonitorInfo
+        {
+            public int cbSize;
+            public Rect rcMonitor;
+            public Rect rcWork;
+            public int dwFlags;
+        }
+    }
+}
---
+++ KasraFbGamesTest/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using nucs.Automation;
+
+namespace KasraFbGamesTest
+{
+    public partial class Form1 : Form
+    {
+        private DirectXManager _dxManager;
+        
+        // vvv You can edit these vvv
+        // A fixed amount to move the mouse vertically each throw, you could make this into a MIN and MAX to do variable amounts in a range.
+        private int FLICK_MOVEMENT_Y = 40;
+        // The max movement to use for the horizontal motion of each throw, how far should the horizontal movement be when the net is on the far-left or far-right?
+        private int FLICK_MOVEMENT_X_MAX = 30;
+        // How big of an area to look around of the detected ball and net
+        private int CROP_IMG_MARGIN = 200;
+
+        // Game specific variables
+        private Rect _ballLastRect;
+        private Rect _netLastRect;
+        private int _lastNetXDelta;
+        private bool _inGame;
+        private int _attemptsMadeThisGame;
+        private DateTime _lastFlickTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _dxManager = new DirectXManager();
+        }
+
+        private async Task<bool> FlickBall(Rect ball, Rect net)
+        {
+            BackColor = Color.HotPink;
+
+            int ballMiddleX = (int)ball.centerX();
+            int ballMiddleY = (int)ball.centerY();
+
+            int netMiddleX = (int)net.centerX();
+            int netMiddleY = (int)net.centerY();
+
+            int diffX = netMiddleX - ballMiddleX;
+            double diffScaled = diffX / (ball.width() * 1.0);
+
+            int maxXMovement = diffScaled > 0.0f ? FLICK_MOVEMENT_X_MAX : -FLICK_MOVEMENT_X_MAX;
+            int movementX = Math.Abs(diffScaled) > 0.1f ? 0 + (int)(maxXMovement * SineEaseInOut(diffScaled * 0.65)) : 0;
+
+            Debug.WriteLine("Shooting: ballMiddleX: {0}, netMiddleX: {1}, diffX: {2}, diffScaled: {3}, movementX: {4}", ballMiddleX, netMiddleX, diffX, diffScaled, movementX);
+
+            Mouse.AbsoluteMove(ballMiddleX, ballMiddleY);
+            Mouse.LeftDown();
+            await Mouse.MoveRelative(movementX, -FLICK_MOVEMENT_Y);
+            Mouse.LeftUp();
+
+            BackColor = DefaultBackColor;
+            _lastFlickTime = DateTime.UtcNow;
+
+            return true;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _dxManager.Init(Handle);
+            _dxManager.StartRecording(() => onFrameUpdate());
+        }
+
+        private async void onFrameUpdate()
+        {
+            var update = _dxManager.GetProcessor().Take();
+
+            Bitmap img = (Bitmap)update.LastAcquiredFrame.Clone();
+            img.Save("last_frame.jpg", ImageFormat.Jpeg);
+
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = "custom_object_detector";
+            compiler.StartInfo.Arguments = ". last_frame.jpg";
+            compiler.StartInfo.UseShellExecute = false;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.StartInfo.CreateNoWindow = true;
+            compiler.Start();
+
+            var output = compiler.StandardOutput.ReadToEnd();
+
+            compiler.WaitForExit();
+            
+            if (compiler.ExitCode == 0 && output.StartsWith("{"))
+            {
+                var res = Jil.JSON.Deserialize<DetectorResponse>(output);
+
+                Rect ball = res.ball;
+                Rect net = res.net;
+
+                var farLeft = net.left < ball.left ? net.left : ball.left;
+                var farRight = net.right > ball.right ? net.right : ball.right;
+
+                var cropLeft = farLeft < CROP_IMG_MARGIN ? farLeft : farLeft - CROP_IMG_MARGIN;
+                var cropRight = farRight > img.Width - CROP_IMG_MARGIN ? farRight : farRight + CROP_IMG_MARGIN;
+
+                var cropTop = net.top < CROP_IMG_MARGIN ? net.top : net.top - CROP_IMG_MARGIN;
+                var cropBottom = ball.bottom > img.Height - CROP_IMG_MARGIN ? ball.bottom : ball.bottom + CROP_IMG_MARGIN;
+
+                var cropRectangle = new Rectangle(cropLeft, cropTop, cropRight - cropLeft, cropBottom - cropTop);
+                var croppedBitmap = img.Clone(cropRectangle, img.PixelFormat);
+
+                if (!_inGame)
+                {
+                    _inGame = true;
+                    _attemptsMadeThisGame = 0;
+                }
+
+                var timeSinceLast = DateTime.UtcNow.Subtract(_lastFlickTime);
+
+                var lastBallXMovement = 0;
+                if (_ballLastRect != null)
+                    lastBallXMovement = Math.Abs(_ballLastRect.left - ball.left);
+
+                var lastNetXDelta = 0;
+                if (_netLastRect != null)
+                    lastNetXDelta = net.left - _netLastRect.left;
+
+                var isNetMovingStage = _inGame && _attemptsMadeThisGame > 10;
+                if (!isNetMovingStage && (lastNetXDelta != 0 && _lastNetXDelta != 0))
+                    isNetMovingStage = true;
+
+                var isNetMovingOverride = false;
+                if (isNetMovingStage)
+                {
+                    int boundsStart;
+                    int boundsEnd;
+                    int comparePoint;
+
+                    if (lastNetXDelta > 0)
+                    {
+                        // net is moving to the right
+                        boundsStart = (int)(ball.left - (ball.width() * 3.0));
+                        boundsEnd = ball.left;
+                        comparePoint = (int)net.centerX();
+                    } else
+                    {
+                        // net is moving to the left
+                        boundsStart = ball.right;
+                        boundsEnd = (int)(ball.right + (ball.width() * 3.0));
+                        comparePoint = (int)net.centerX();
+                    }
+
+                    Debug.WriteLine("\tisNetMovingStage, compareX: {0}, boundsStart: {1}, boundsEnd: {2}", comparePoint, boundsStart, boundsEnd);
+                }
+
+                var ballDiffToNet = ball.bottom - net.bottom;
+
+                var timeBeforeNextShot = isNetMovingStage ? 5000 : 3000;
+
+                // Only if we haven't done anything in at least timeBeforeNextShot milliseconds, and the ball hasn't moved much since last frame, and the net hasn't moved (or it's in the moving stage and we can shoot)
+                if (timeSinceLast.TotalMilliseconds > timeBeforeNextShot && lastBallXMovement < 5 && ballDiffToNet > 100 && (lastNetXDelta == 0 || (isNetMovingStage || isNetMovingOverride)))
+                {
+                    var destination = net;
+                    if (isNetMovingOverride)
+                    {
+                        destination = new Rect();
+                        destination.left = ball.left;
+                        destination.right = ball.right;
+                    }
+                    // To update the GUI
+                    pictureBox1.Image = croppedBitmap;
+                    // To actually flick the mouse as needed and wait until it's done
+                    await FlickBall(ball, destination);
+                } else
+                {
+                    Debug.WriteLine("Not updating, timeSinceLast: {0}, lastBalLXDelta: {1}, lastNetXDelta:  {2}, isNetMovingStage: {3}", timeSinceLast.TotalMilliseconds, lastBallXMovement, lastNetXDelta, isNetMovingStage);
+                }
+
+                _lastNetXDelta = lastNetXDelta;
+                _netLastRect = net;
+                _ballLastRect = ball;
+            }
+            else
+            {
+                if (_inGame)
+                {
+                    _inGame = false;
+                    _attemptsMadeThisGame = 0;
+                }
+            }
+
+            await Task.Delay(50);
+            _dxManager.GetRecorder().Add(update);
+
+            img.Dispose();
+        }
+
+        public static float SineEaseInOut(double s)
+        {
+            return (float)(Math.Sin(s * (float)Math.PI - (float)(Math.PI / 2)) + 1) / 2;
+        }
+
+        private class Rect
+        {
+            public int left { get; set; }
+            public int top { get; set; }
+            public int right { get; set; }
+            public int bottom { get; set; }
+
+            public int width()
+            {
+                return right - left;
+            }
+
+            public int height()
+            {
+                return bottom - top;
+            }
+
+            public double centerX()
+            {
+                return left + ((right - left) / 2.0);
+            }
+
+            public double centerY()
+            {
+                return top + ((bottom - top) / 2.0);
+            }
+        }
+
+        private class DetectorResponse {
+            public Rect ball { get; set; }
+            public Rect net { get; set; }
+        }
+    }
+}
